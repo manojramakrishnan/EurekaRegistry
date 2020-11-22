cfd81fa1821065a784b34de8155f3c51e1f3bcd4
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2020-11-22 16:07:50 +0530

    Service Discovery with Eureka related complete code commit

---
+++ Flipcart-Server/src/main/java/com/javaevangelist/eureka/api/FlipcartServerApplication.java
@@ -0,0 +1,14 @@
+package com.javaevangelist.eureka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FlipcartServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipcartServerApplication.class, args);
+	}
+}
